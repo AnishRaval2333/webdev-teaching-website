dc190c3fb15bf9bc235b4f3fbdaaefbb7e1233df
Eetu Karttunen <Eetu.Karttunen@edu.karelia.fi>
2021-06-12 10:22:03 +0300

    'init2'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pohja valmis
+          Pohja valmis, muttei ihan vielä
         </p>
         <a
           className="App-link"
